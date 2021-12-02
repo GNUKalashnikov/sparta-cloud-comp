86a7ac09e44493bc79b775c67df66c98e923d5f1
Ivan Kalashnikov <ivan@kalashnikov.uk>
2021-12-02 12:18:48 +0000

    working aws server and app

---
+++ provision.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# A Bunch of ubuntu based commands for a quick set up
+
+# Initial setup
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Specific install of nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# Dependencies
+sudo apt-get install git -y
+sudo apt-get install python-software-properties
+
+# install pm2
+sudo mpm instoall pm2 -g
+
+# setting DB
+echo "export DB_HOST=DB_HOST="mongodb://TheMcIp:27017/posts >> ~/.bashrc
